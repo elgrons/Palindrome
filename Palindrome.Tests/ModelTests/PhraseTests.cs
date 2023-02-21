2b926bbbaf78d80f17915ab4517ae870ca7b4de0
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-21 10:03:50 -0800

    first test passes, Phase Constructor created.
    Co-authored-by: Hyung Jin Kim<hyungjinkim1205@gmail.com
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>
    Co-authored-by: Luckie <eluckie.d@gmail.com>”

---
+++ Palindrome.Tests/ModelTests/PhraseTests.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindrome.Models;
+using System.Collections.Generic; 
+using System;
+
+namespace Palindrome.Tests
+{
+  [TestClass]
+  public class PhraseTests
+  {
+        [TestMethod]
+    public void PhraseConstructor_CreatesInstanceOfPhrase_Phrase()
+
+    {
+      Phrase newPhrase = new Phrase();
+      Assert.AreEqual(typeof(Phrase), newPhrase.GetType());
+    }
+    [TestMethod]
+    public void PhraseConstructor_CreatesInstanceOfPhrase_Phrase()
+
+    {
+      // Arrange
+      string Word = "racecar";
+      Phrase newPhrase = new Phrase("racecar");
+      // Act
+      string result = newPhrase.Word;
+      // Assert
+      Assert.AreEqual(Word, result);
+    }
+  }
+}
