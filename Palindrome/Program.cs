bfb97a8ce521dd3a3caed534442fb3f7fb1f7524
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-21 15:30:32 -0800

    WIP: UI.
    Co-authored-by: Hyung Jin Kim<hyungjinkim1205@gmail.com
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>
    Co-authored-by: Luckie <eluckie.d@gmail.com>

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -7,12 +7,17 @@ namespace Palindrome
   {
     static void Main()
     {
-      // UI code goes here.
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to the Palindrome app!");
+      Console.WriteLine("We'll decide if you have submitted a palindrome based off of your input.");
+      Console.WriteLine("Please enter a word:");
+      string word = Console.ReadLine();
+      Phrase newPhrase = new Phrase();
+      newPhrase.IsPhrase(word);
+      newPhrase.IsReversePhrase(word);
+      newPhrase.IsReversePhraseString(word);
+      bool result = newPhrase.BePalindrome(word, result);
+      Console.WriteLine("Is " + word + " a palindrome? " + result);
     }
-
-
-
-
-
   }
 }
